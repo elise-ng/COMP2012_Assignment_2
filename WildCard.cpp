cc913d2687f80f42896eaf8a6f03c0cc2607a5fa
Elliot Ng <chihimng.io@gmail.com>
2018-04-14 07:46:52 +0800

    implement wildcard, use own point const for drawtwocard

--- WildCard.cpp
+++ WildCard.cpp
@@ -8,3 +8,45 @@
 #include "WildCard.h"
 #include "Player.h"
 
+WildCard::WildCard() : Card(Color::meta, POINT_WILDCARD) {
+}
+
+WildCard::~WildCard() {
+  this->Card::~Card();
+}
+
+bool WildCard::operator^(const Card& t) const {
+  return true;
+}
+
+void WildCard::castEffect(Player *&currentPlayer, CardPile &drawPile, CardPile &discardPile) {
+  this->color = currentPlayer->chooseColor();
+}
+
+void WildCard::init() {
+  this->color = Color::meta;
+}
+
+void WildCard::serialize(std::ostream &os) const {
+  if (this->color == Color::meta) {
+    os << "WC";
+  } else {
+    switch (this->color) {
+      case Color::red:
+        os << 'R';
+        break;
+      case Color::blue:
+        os << 'B';
+        break;
+      case Color::green:
+        os << 'G';
+        break;
+      case Color::yellow:
+        os << 'Y';
+        break;
+      default:
+        break;
+    }
+    os << 'w';
+  }
+}
--- WildCard.h
+++ WildCard.h
@@ -8,5 +8,18 @@
 #ifndef WILDCARD_H_
 #define WILDCARD_H_
 
+#include "Card.h"
+
+class WildCard : public Card {
+public:
+  WildCard();
+  virtual ~WildCard() override;
+  virtual bool operator^(const Card& t) const override;
+  virtual void castEffect(Player*& currentPlayer, CardPile& drawPile, CardPile& discardPile) override;
+  virtual void init() override;
+protected:
+  virtual void serialize(ostream& os) const override;
+private:
+};
 
 #endif /* WILDCARD_H_ */
