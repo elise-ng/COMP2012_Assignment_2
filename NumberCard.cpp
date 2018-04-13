6016d0ddd34f0cf7ad32b37aed944496491ca485
Elliot Ng <chihimng.io@gmail.com>
2018-04-14 06:00:01 +0800

    implement numbercard

--- NumberCard.cpp
+++ NumberCard.cpp
@@ -7,3 +7,40 @@
 
 #include "NumberCard.h"
 
+NumberCard::NumberCard(int number, Color color) : Card(color, number), number(number) {
+}
+
+NumberCard::~NumberCard() : ~Card() {
+};
+
+bool NumberCard::operator^(const Card& t) const {
+  if (NumberCard & numT = dynamic_cast<NumberCard>(t) && numT->number == this->number) {
+    return true;
+  } else {
+    return this->Card::operator^(t)
+  }
+}
+
+void NumberCard::castEffect(Player*& currentPlayer, CardPile& drawPile, CardPile& discardPile) {
+  // no action for number card
+}
+
+void NumberCard::serialize(std::ostream& os) {
+  switch (this->color) {
+    case Color::red:
+      os << "B";
+      break;
+    case Color::blue:
+      os << "B";
+      break;
+    case Color::green:
+      os << "B";
+      break;
+    case Color::yellow:
+      os << "B";
+      break;
+    default:
+      break;
+  }
+  os << this->number;
+}
--- NumberCard.h
+++ NumberCard.h
@@ -8,5 +8,18 @@
 #ifndef NUMBERCARD_H_
 #define NUMBERCARD_H_
 
+#include "Card.h"
+using namespace std;
+
+class NumberCard : public Card {
+public:
+  NumberCard(int, Color);
+  ~NumberCard();
+  virtual bool operator^(const Card& t) const;
+  virtual void castEffect(Player*& currentPlayer, CardPile& drawPile, CardPile& discardPile);
+  virtual void serialize(ostream& os) const;
+private:
+  const int number;
+};
 
 #endif /* NUMBERCARD_H_ */
