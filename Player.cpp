31906494630be937e42d92ab7ec8467dd78d8678
Elliot Ng <chihimng.io@gmail.com>
2018-04-14 09:04:06 +0800

    implement player

--- Player.cpp
+++ Player.cpp
@@ -15,3 +15,32 @@ ostream& operator<<(ostream& os, const Player& h) {
 	return os;
 }
 
+Player::Player(string name, Player* previous) : name(name) {
+}
+
+void Player::drawCard(CardPile &drawPile, CardPile &discardPile, int number_of_cards) {
+	while (number_of_cards > 0) {
+		if (drawPile.getSize() <= 0) {
+			for (int i = discardPile.getSize()-1-1; i >= 0; i += 1) {
+				drawPile += discardPile.removeCard(i);
+			}
+			drawPile.shuffle();
+		}
+		*this += drawPile.removeTopCard();
+		number_of_cards -= 1;
+	}
+}
+
+Card* Player::playCardAfter(const Card* topCard, int index) {
+	const Card* card = this->getCard(index);
+	if (card == nullptr || !(*topCard ^ *card)) { return nullptr; }
+	return this->removeCard(index);
+}
+
+int Player::getScore() const {
+	int total = 0;
+	for (int i = 0; i < this->getSize(); i += 1) {
+		total += this->getCard(i)->getPoint();
+	}
+	return total;
+}
